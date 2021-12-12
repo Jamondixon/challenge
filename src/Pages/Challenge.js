2097f16976fe826029009a5012b37cd5a51b3091
Jamon Dixon <jamonadixon@gmail.com>
2021-12-11 18:45:25 -0600

    icons

--- src/Pages/Challenge.js
+++ src/Pages/Challenge.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import './Challenge.scss'
-import backdrop from '../Images/Coding-Challenge-Image.png'
-
+import plus from '../Images/Plus.png'
+import instagram from '../Images/Instagram.png'
+import facebook from '../Images/Facebook.png'
 
 export default function Challenge() {
     return (
         <div className="challenge-container">
             <div className="title-container">
-                <h1 className="title">Explore</h1>
+                <h1 className="text-white">Explore</h1>
             </div>
-            <div className="flex h-100 w-100">
-                <img src={backdrop}  alt="sun through trees" className="trees"/>
+            <img src={plus} alt="plus sign" className="plus-icon"/>
+            <img src={facebook} alt="facebook" className="facebook-icon"/>
+            <img src={instagram} alt="instagram" className="instagram-icon"/>
+            <div className="sidebar w-25">
             </div>
-            <nav className=" bg-dark col-md-4">
-                <div className="sidebar">
-                </div>
-            </nav>
         </div>
     )
 }
