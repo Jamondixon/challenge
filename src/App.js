c9a93f1fd7c220c83ee5efd5ce87f242b6c8a3ba
Jamon Dixon <jamonadixon@gmail.com>
2021-12-13 19:08:17 -0600

    content box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Challenge from './Pages/Challenge';
+import React from 'react'
+
 
 function App() {
+
+  
   return (
     <div className="App">
       <Challenge />
--- src/Pages/Challenge.js
+++ src/Pages/Challenge.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Challenge.scss'
 import plus from '../Images/Plus.png'
 import instagram from '../Images/Instagram.png'
 import facebook from '../Images/Facebook.png'
+import Content from '../Components/Content'
 
 export default function Challenge() {
+
+    const [readMore, setReadMore] = useState(false);
+    // const moreInfo = <div className="content-container">
+    //                     <h1 className="expanded-title">Explore</h1>
+    //                     <hr className="gradient-line" />
+    //                     <p className="expanded-content">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
+    //                     <button>Read More</button>
+    //                 </div>
+
+
     return (
         <div className="challenge-container">
             <div className="title-container">
                 <h1 className="text-white">Explore</h1>
             </div>
-            <div className="sidebar w-25">
+            <div className="sidebar w-25"></div>
             <p className="text-white">More Details</p>
-            <img src={plus} alt="plus sign" className="plus-icon"/>
+            <button  onClick={()=>{setReadMore(!readMore)}}><img src={plus} src={plus} alt="plus icon" className="plus-icon"/></button>
+            {readMore && <Content />}
             <img src={facebook} alt="facebook" className="facebook-icon"/>
             <img src={instagram} alt="instagram" className="instagram-icon"/>
-            </div>
         </div>
     )
 }
