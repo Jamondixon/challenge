df313eafc032185ec956e80f872f809efa54969e
Jamon Dixon <jamonadixon@gmail.com>
2021-12-17 09:15:35 -0600

    before cleanup

--- src/Pages/Challenge.js
+++ src/Pages/Challenge.js
@@ -41,11 +41,11 @@ export default function Challenge(props) {
                 <div className="title-container">
                     <h1 className="text-white">Explore</h1>
                 </div>
-                <p className="text-white">More Details</p>
+                <p className="text-white details">More Details</p>
             </div>
             <div className="sidebar w-25"></div>
             <div className="content-container">
-                <h3 className="exit-icon" onClick={ hideContentInfo }><BsXLg/></h3>
+                <h3 className="exit-icon" onClick={hideContentInfo}><BsXLg/></h3>
                 <h2 className="content-title">Explore</h2>
                 <hr className="gradient-line" />
                 <p className="content">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? </p>
@@ -53,7 +53,6 @@ export default function Challenge(props) {
                     <button className="text-white read-more">READ MORE</button>
                 </div>
             </div>
-            {/* <div className="card"></div> */}
             <button  onClick={toggleBox}><img src={plus} alt="plus icon" className="plus-icon"/></button>
             <img src={facebook} alt="facebook" className="facebook-icon"/>
             <img src={instagram} alt="instagram" className="instagram-icon"/>
